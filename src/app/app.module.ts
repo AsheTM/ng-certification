b108dce8ff23a6776dfe87797f3e554e8e6cac52
AsheTM <haithemalidaoud@gmail.com>
2021-06-29 19:39:09 +0100

    Modify app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { CoreModule } from './core';
+import { SharedModule } from './shared';
+
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [
+    BrowserModule, 
+
+    AppRoutingModule, 
+
+    CoreModule.forRoot(environment.configuration.core), 
+    SharedModule.forRoot({
+      ...environment.configuration.shared, 
+      http:         {
+        apiKey: environment.configuration.shared.http.apiKey,
+        param:  environment.configuration.shared.http.param,
+        url:    environment.configuration.shared.http.urls.weather
+      }
+    })
+  ],
+  declarations: [AppComponent],
+  bootstrap:    [AppComponent]
 })
 export class AppModule { }
