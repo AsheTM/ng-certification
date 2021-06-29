fd0f6ee781a990ef0a25c03f2a294d5317c2762b
AsheTM <haithemalidaoud@gmail.com>
2021-06-29 19:38:11 +0100

    Create app route file

---
+++ src/app/app.route.ts
@@ -0,0 +1,18 @@
+
+namespace Route {
+    export const ROOT:          string = '';
+    export const WILDCARD:      string = '**';
+    export const REDIRECT_TO:   string = '/';
+
+    export namespace Feature {
+        export const ZIPCODE:   string = '';
+        export const FORECAST:  string = 'forecast';
+    }
+}
+
+export const APP_ROUTE_ROOT:                string = Route.ROOT;
+export const APP_ROUTE_WILDCARD:            string = Route.WILDCARD;
+export const APP_ROUTE_REDIRECT_TO:         string = Route.REDIRECT_TO;
+
+export const APP_ROUTE_FEATURE_ZIPCODE:     string = Route.Feature.ZIPCODE;
+export const APP_ROUTE_FEATURE_FORECAST:    string = Route.Feature.FORECAST;
