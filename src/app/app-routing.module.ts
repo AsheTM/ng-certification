acc4e0e22ff641b5c31596ba3830eece0c91b17d
AsheTM <haithemalidaoud@gmail.com>
2021-06-29 19:38:40 +0100

    Create app routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import {
+    APP_ROUTE_ROOT, 
+    APP_ROUTE_FEATURE_FORECAST, 
+    APP_ROUTE_FEATURE_ZIPCODE, 
+    APP_ROUTE_REDIRECT_TO, 
+    APP_ROUTE_WILDCARD
+} from './app.route';
+
+import { environment } from 'src/environments/environment';
+
+
+const routes: Route[] = [
+    {
+        path:       APP_ROUTE_ROOT, 
+        component:  AppComponent, 
+        children:   [
+            { path: APP_ROUTE_FEATURE_ZIPCODE,  loadChildren: () => import('./zipcode').then(m => m.ZipcodeModule) }, 
+            { path: APP_ROUTE_FEATURE_FORECAST, loadChildren: () => import('./forecast').then(m => m.ForecastModule) }
+        ]
+    }, {
+        path:       APP_ROUTE_WILDCARD, 
+        redirectTo: APP_ROUTE_REDIRECT_TO
+    }
+]
+
+@NgModule({
+  imports:  [RouterModule.forRoot(routes, {
+        enableTracing: !environment.production
+  })], 
+  exports:  [RouterModule]
+})
+export class AppRoutingModule { }
