0c96bf9e0f2c8ba6b4ed3f57034342c487e479ea
AsheTM <haithemalidaoud@gmail.com>
2021-08-17 13:30:18 +0100

    Create interval shared service

--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -1,3 +1,4 @@
 
 export * from './http.service';
+export * from './interval.service';
 export * from './local-storage.service';
---
+++ src/app/shared/services/interval.service.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from '@angular/core';
+import { iif, Observable, of, timer } from 'rxjs';
+
+import { SharedModule } from '../shared.module';
+import { SHARED_TOKEN_VALUE_HTTP } from '../shared.token';
+import { TSharedModuleConfigurationHttp } from '../shared.type';
+
+
+@Injectable({
+  providedIn: SharedModule
+})
+export class IntervalService {
+
+  private readonly INTERVAL:  number | undefined  = this._sharedTokenValueHttp.interval;
+  
+  interval$:  Observable<void>  = iif(() => this.INTERVAL !== undefined, timer(0, this.INTERVAL), of(null));
+
+  constructor(
+    @Inject(SHARED_TOKEN_VALUE_HTTP) 
+      private _sharedTokenValueHttp:  TSharedModuleConfigurationHttp
+  ) { }
+
+}
