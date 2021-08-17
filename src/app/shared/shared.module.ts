deff7be87d40af5abf94f8974bcbf08d8d29a616
AsheTM <haithemalidaoud@gmail.com>
2021-08-17 13:11:59 +0100

    Add check in constructor of SharedModule if forRoot and forFeature are called

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -37,19 +37,52 @@ import { CacheInterceptor } from './interceptors';
       provide:  HTTP_INTERCEPTORS, 
       useClass: CacheInterceptor, 
       multi:    true
+    }, 
+
+    {
+      provide:  SHARED_PROVIDER_FOR_ROOT,
+      useValue: ESharedProvider.NO_STATIC_METHODS_INVOKED
+    }, {
+      provide:  SHARED_PROVIDER_FOR_FEATURE,
+      useValue: ESharedProvider.NO_STATIC_METHODS_INVOKED
     }
   ]
 })
 export class SharedModule {
 
-  static forRoot({ http, interceptor, storage }: Pick<TSharedModuleConfiguration, 'http' | 'interceptor' | 'storage'>): ModuleWithProviders<SharedModule> {
+  constructor(
+    @Self()
+    @Inject(SHARED_PROVIDER_FOR_ROOT)
+      private _forRoot: number,
+    @SkipSelf()
+    @Optional()
+    @Inject(SHARED_PROVIDER_FOR_ROOT)
+      private _forRootInFeature: number,
+    @Self()
+    @Inject(SHARED_PROVIDER_FOR_FEATURE)
+      private _forFeature: number,
+  ) {
+    switch(true) {
+      case this._forRoot === ESharedProvider.NO_STATIC_METHODS_INVOKED && this._forRootInFeature === null:
+        throw new Error(`[${this.constructor.name}] You must call the \'forRoot\' static method in app.module.ts!`);
+      case this._forRootInFeature === ESharedProvider.FOR_ROOT && this._forFeature === ESharedProvider.NO_STATIC_METHODS_INVOKED:
+        throw new Error(`[${this.constructor.name}] You must call the \'forFeature\' static method in that feature module!`);
+    }
+  }
+
+  static forRoot({
+    interceptor, 
+    storage
+  }: Pick<TSharedModuleConfiguration, 'interceptor' | 'storage'>): ModuleWithProviders<SharedModule> {
     return {
       ngModule: SharedModule, 
       providers: [
         {
-          provide:  SHARED_TOKEN_VALUE_HTTP, 
-          useValue: http
-        }, {
+          provide:  SHARED_PROVIDER_FOR_ROOT,
+          useValue: ESharedProvider.FOR_ROOT
+        }, 
+
+        {
           provide:  SHARED_TOKEN_VALUE_INTERCEPTOR, 
           useValue: interceptor
         }, {
@@ -60,10 +93,18 @@ export class SharedModule {
     };
   }
 
-  static forFeature({ http, fallback }: Pick<TSharedModuleConfiguration, 'http' | 'fallback'>): ModuleWithProviders<SharedModule> {
+  static forFeature({
+    http, 
+    fallback
+  }: Pick<TSharedModuleConfiguration, 'http' | 'fallback'>): ModuleWithProviders<SharedModule> {
     return {
       ngModule: SharedModule, 
       providers: [
+        {
+          provide:  SHARED_PROVIDER_FOR_FEATURE,
+          useValue: ESharedProvider.FOR_FEATURE
+        }, 
+
         {
           provide:  SHARED_TOKEN_VALUE_HTTP, 
           useValue: http
